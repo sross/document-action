b9c08dca73fcb4a541c7e8737b19472e9500496c
Sean Ross <sross@hacktar.local>
2008-10-09 12:43:12 +0100

    bump version

--- sysdef.lisp
+++ sysdef.lisp
@@ -1,7 +1,7 @@
 (in-package :sysdef-user)
 
 (define-system :document-action (serial-system)
-  (:version 0 1 1)
+  (:version 0 1 2)
   (:components "package" "utils" "action" "render")
   (:requires :alexandria :cl-who :cl-ppcre)
   (:preferences "~/.mudballs.prefs"))
